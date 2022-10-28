eab25262af0b11a23a4c5d30c33977adb62672d8
Fidel Coronel <fidel.coronel@tensoriot.com>
2022-10-28 16:28:13 -0700

    push iframe changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,8 +5,17 @@ function landingPage() {
     <div>
       <p>landingPage Working</p>
       <a href='protected'>Protected Page Link</a>
+      <div>
+        <p>Working Iframe</p>
+        <iframe src="https://www.youtube.com/embed/rncjTx-rRVc" title="Iframe Example" width="500" height="500"></iframe>
+      </div>
+      <div>
+        <p>https://stackoverflow.com/a/27359037/12964268    xframe error on firefox.  On chrome CSP: frame-ancestor problem </p>
+        <iframe id="test" src="https://x9rq0nl12x.labeling.us-east-1.sagemaker.aws/#/workstreams" title="Iframe Example" width="500" height="500"></iframe>
+      </div>
     </div>
   )
 }
+// src='https://x9rq0nl12x.labeling.us-east-1.sagemaker.aws/#/workstreams' 
 
 export default landingPage
